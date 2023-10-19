abdc0b98433ca095036c597e81e6760f7d9abdfa
18iRi81 <147032320+18iRi81@users.noreply.github.com>
2023-10-19 12:59:04 +0200

    1st

---
+++ wdp2.py
@@ -0,0 +1,21 @@
+"""
+imie = input('podaj imie')
+print(f'twoje imie  to ' + imie)
+
+wiek = (input('podaj wiek'))
+print(f'twoj wiek to ' + wiek)
+
+print('ala' + 'ma' + 'kota')
+print(3*'ala')
+"""
+fraza ='wdp jest super'
+print(fraza)
+print(fraza[:3]) #3 pierwsze
+#print(fraza[3:]) 3 ostatnie
+#print(fraza[3:2]) od 3rd do 2nd
+#print(fraza[-3:]) ?
+#print(fraza[-1]) ?
+#print(fraza[:-3]) ?
+
+
+print(fraza[0:4] + 'JEST' + fraza[8:14])
