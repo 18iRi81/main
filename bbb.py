056236dfbffef61e60f054e7e3321721ca1296f7
18iRi81 <147032320+18iRi81@users.noreply.github.com>
2023-10-26 01:46:29 +0200

    Add files via upload
    printy

---
+++ bbb.py
@@ -0,0 +1,27 @@
+"""
+imie = input('podaj imie')
+print(f'twoje imie  to ' + imie)
+
+wiek = (input('podaj wiek'))
+print(f'twoj wiek to ' + wiek)
+
+print('ala' + 'ma' + 'kota')
+print(3*'ala')
+"""
+fraza ='wdp jest super'
+print(fraza)
+print('________________')
+print(fraza[:3])#3 pierwsze
+print('________________')
+print(fraza[3:])# bez 3 pierwszych
+print('________________')
+print(fraza[3:6]) #od 3rd do 2nd
+print('________________')
+print(fraza[-3:]) #3 ostatnie
+print('________________')
+print(fraza[-2]) # jedna litera od konca
+print('________________')
+print(fraza[:-3]) #wsystko poza 3 ostatnimi
+print('________________')
+
+print(fraza[0:4] + 'JEST' + fraza[8:14])
