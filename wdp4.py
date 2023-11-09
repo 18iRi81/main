19dd0b1e00cba343e76526db0a5eab1232d4ec27
18iRi81 <147032320+18iRi81@users.noreply.github.com>
2023-11-09 12:57:24 +0100

    dunno

---
+++ wdp4.py
@@ -0,0 +1,110 @@
+#zadanie 1
+"""
+lista = []
+for i in range(1, 11):
+    lista.append(i)
+print(lista)
+"""
+"""
+lista = []
+for i in range(0,21,2):
+    lista.append(i)
+print(lista)
+"""
+"""
+lista = []
+for i in range(1, 11):
+    lista.append(i*i)
+print(lista)
+"""
+"""
+lista = []
+for i in range(1, 11):
+    lista.append(i*0)
+print(lista)
+"""
+"""
+lista = []
+for i in range(0, 10, ):
+    lista.append(i%2)
+print(lista)
+"""
+"""
+lista = []
+for i in range(1, 11):
+    lista.append(i%5)
+print(lista)
+"""
+
+
+
+#zadanie 2
+"""
+lista = []
+i = 1
+while i<=10:
+    lista.append(i)
+    i += 1
+print(lista)
+"""
+"""
+lista = []
+i = 0
+while i <= 20:
+    if i % 2:
+        lista.append(i)
+    i += 1
+print(lista)
+"""
+"""
+lista = []
+i = 1
+while i <= 10:
+    lista.append(i*i)
+    i += 1
+print(lista)
+"""
+"""
+lista = []
+i = 0
+while i <= 10:
+    lista.append(i*0)
+    i += 1
+print(lista)
+"""
+"""
+lista = []
+i = 0
+while i <= 10:
+    lista.append(i%2)
+    i += 1
+print(lista)
+"""
+"""
+lista = []
+i = 0
+while i <= 9:
+    lista.append(i%5)
+    i += 1
+print(lista)
+"""
+
+#zadanie 3
+"""
+lista = [-1, -2, -3, -4, -8, 5, 6]
+r = 0
+for i in lista:
+    if i < 0:
+        r += 1
+print(r)
+"""
+
+#zadanie 4
+lista = [-1, -2, -3, -4, -8, 5, 6]
+r = 1
+for i in lista:
+    r = r * i
+print(r)
+
+
+
