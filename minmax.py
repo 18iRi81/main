3b4657e81ca15b0a01e3bcf3f7d98fe12d825c0a
18iRi81 <147032320+18iRi81@users.noreply.github.com>
2023-11-30 03:15:19 +0100

    Add files via upload

---
+++ minmax.py
@@ -0,0 +1,15 @@
+lista = [1 ,0, -5, 6, -18, 22, -88]
+def min(lista):
+    a = lista[0]
+    for i in lista:
+        a = i
+        return a
+        
+"""
+def max(lista):
+a = lista[0]
+for i in lsita:
+if i > a:
+a = i
+return a
+"""
